9038668bd3b48561f89af3be8c199a1fd1916af2
h-jun01 <jyun.0110@icloud.com>
2021-11-01 22:38:47 +0900

    refactor: Delete unnecessary files.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'styles/theme';
 import { GlobalStyle } from 'styles/globalStyle';
@@ -16,8 +14,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
