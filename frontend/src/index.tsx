f61309d382eeaff0a437235a3ab2bd6944675c99
h-jun01 <jyun.0110@icloud.com>
2021-11-01 22:34:31 +0900

    feat: Add themes and global styles

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'styles/theme';
+import { GlobalStyle } from 'styles/globalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
