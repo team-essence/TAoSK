47f5547b7f3093a7fd809de1e534f78070415f69
roll1226 <tomo_yasu_4724@icloud.com>
2021-11-01 21:49:24 +0900

    test: husky

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: FC = () => {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
