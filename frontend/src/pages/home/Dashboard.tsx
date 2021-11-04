8a212ac1fa9bc8c415cef8628843accbacfaab1b
roll1226 <tomo_yasu_4724@icloud.com>
2021-11-04 16:59:36 +0900

    test: lint-staged

--- frontend/src/pages/home/Dashboard.tsx
+++ frontend/src/pages/home/Dashboard.tsx
@@ -7,32 +7,32 @@ import { useQuery } from '@apollo/client';
 export const Dashboard: FC = () => {
   const { user } = useAuthContext();
 
-  const todosQuery = useMemo(
-    () => gql`
-      {
-        todos {
-          name
-          id
-        }
-      }
-    `,
-    []
-  );
+  // const todosQuery = useMemo(
+  //   () => gql`
+  //     {
+  //       todos {
+  //         name
+  //         id
+  //       }
+  //     }
+  //   `,
+  //   []
+  // );
 
   // クエリを発行する
-  const { loading, error, data } = useQuery(todosQuery);
+  // const { loading, error, data } = useQuery(todosQuery);
 
   // 結果を表示する
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  // if (loading) return <p>Loading...</p>;
+  // if (error) return <p>Error :(</p>;
 
-  const { todos } = data;
+  // const { todos } = data;
 
   if (!user) return <Navigate to="/signup" />;
 
   return (
     <div>
-      {JSON.stringify(todos)}
+      {/* {JSON.stringify(todos)} */}
       <p>プロジェクト一覧とか通知とかマイページとか</p>
     </div>
   );
