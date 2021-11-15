3c901d629fd4d1cf9551edbf835d1c9746e60a2e
h-jun01 <jyun.0110@icloud.com>
2021-11-15 20:06:56 +0900

    feat: create wrapper

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,20 +11,23 @@ import { ApolloProvider } from '@apollo/client'
 import { client } from './ApolloClient'
 import { ProjectDetail } from 'pages/projectList/projectDetail/ProjectDetail'
 import { ToastContainer } from 'react-toastify'
+import { ContentWrapper } from 'components/wrapper/ContentWrapper'
 
 const App: FC = () => {
   return (
     <AuthProvider>
       <ApolloProvider client={client}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<ProjectList />} />
-            <Route path="/projects/:id" element={<ProjectDetail />} />
-            <Route path="/mypage/:id" element={<MyPage />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/signin" element={<SignIn />} />
-          </Routes>
-        </BrowserRouter>
+        <ContentWrapper>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<ProjectList />} />
+              <Route path="/projects/:id" element={<ProjectDetail />} />
+              <Route path="/mypage/:id" element={<MyPage />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/signin" element={<SignIn />} />
+            </Routes>
+          </BrowserRouter>
+        </ContentWrapper>
       </ApolloProvider>
     </AuthProvider>
   )
