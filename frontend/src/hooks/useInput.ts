36338986111875ffdea16065dcebd70043f6a174
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2021-11-17 10:13:09 +0900

    improve: useInputにanyがあったので修正 #45

--- frontend/src/hooks/useInput.ts
+++ frontend/src/hooks/useInput.ts
@@ -2,11 +2,15 @@ import { ChangeEvent, useState } from 'react'
 
 export type UseInputResult = {
   value: string
-  onChange: (e: any) => void
+  onChange: (e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => void
 }
 
 export const useInput = (initialValue: string): UseInputResult => {
   const [value, setValue] = useState<string>(initialValue)
 
-  return { value, onChange: (e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value) }
+  return {
+    value,
+    onChange: (e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) =>
+      setValue(e.target.value),
+  }
 }
