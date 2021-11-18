b1494549c888c866f8960c40ef1d9edfd19f5e56
h-jun01 <jyun.0110@icloud.com>
2021-11-19 01:30:47 +0900

    fix: 初回マイページ遷移時にcurrentuserがundefinedになる問題を修正

--- frontend/src/pages/mypage/MyPage.tsx
+++ frontend/src/pages/mypage/MyPage.tsx
@@ -1,22 +1,20 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC, useEffect } from 'react'
 import { Navigate, useParams } from 'react-router-dom'
 import { useAuthContext } from 'context/AuthProvider'
 import { useGetUserLazyQuery } from './mypage.gen'
 
 export const MyPage: FC = () => {
-  const [isMounted, setisMounted] = useState<boolean>(false)
   const { currentUser } = useAuthContext()
   const { id } = useParams()
   const [getUserById, userQuery] = useGetUserLazyQuery()
 
   useEffect(() => {
     if (!currentUser) return
-    setisMounted(true)
     getUserById({ variables: { id: currentUser.uid } })
   }, [currentUser, getUserById])
 
   if (!currentUser) return <Navigate to="/signup" />
-  if (isMounted && currentUser.uid !== id) return <Navigate to="/" />
+  if (currentUser.uid !== id) return <Navigate to="/" />
 
   return (
     <div>
