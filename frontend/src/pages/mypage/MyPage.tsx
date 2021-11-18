b1bfc07ff9fa2f2164efdfbf2437529ab3778225
h-jun01 <jyun.0110@icloud.com>
2021-11-19 00:27:27 +0900

    refactor: mypage eslint

--- frontend/src/pages/mypage/MyPage.tsx
+++ frontend/src/pages/mypage/MyPage.tsx
@@ -1,27 +1,27 @@
-import React, { FC, useEffect, useState } from 'react';
-import { Navigate, useParams } from 'react-router-dom';
-import { useAuthContext } from 'context/AuthProvider';
-import { useGetUserLazyQuery } from './mypage.gen';
+import React, { FC, useEffect, useState } from 'react'
+import { Navigate, useParams } from 'react-router-dom'
+import { useAuthContext } from 'context/AuthProvider'
+import { useGetUserLazyQuery } from './mypage.gen'
 
 export const MyPage: FC = () => {
-  const [isMounted, setisMounted] = useState<boolean>(false);
-  const { currentUser } = useAuthContext();
-  const { id } = useParams();
-  const [getUserById, userQuery] = useGetUserLazyQuery();
+  const [isMounted, setisMounted] = useState<boolean>(false)
+  const { currentUser } = useAuthContext()
+  const { id } = useParams()
+  const [getUserById, userQuery] = useGetUserLazyQuery()
 
   useEffect(() => {
-    if (!currentUser) return;
-    setisMounted(true);
-    getUserById({ variables: { id: currentUser.uid } });
-  }, [currentUser, getUserById]);
+    if (!currentUser) return
+    setisMounted(true)
+    getUserById({ variables: { id: currentUser.uid } })
+  }, [currentUser, getUserById])
 
-  if (!currentUser) return <Navigate to="/signup" />;
-  if (isMounted && currentUser.uid !== id) return <Navigate to="/" />;
+  if (!currentUser) return <Navigate to="/signup" />
+  if (isMounted && currentUser.uid !== id) return <Navigate to="/" />
 
   return (
     <div>
       <p>マイページ</p>
       <pre>{JSON.stringify(userQuery.data?.user, null, '\t')}</pre>
     </div>
-  );
-};
+  )
+}
