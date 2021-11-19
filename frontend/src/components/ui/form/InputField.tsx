bbd3a3c5255a2b5132e1fcf15b5b7269dc843df9
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2021-11-19 13:15:30 +0900

    add: InputFieldのlabelとinputのstyleをpropsで渡せるようにした #45

--- frontend/src/components/ui/form/InputField.tsx
+++ frontend/src/components/ui/form/InputField.tsx
@@ -2,8 +2,19 @@ import React, { FC, InputHTMLAttributes } from 'react'
 import { UseFormRegisterReturn, FieldError } from 'react-hook-form'
 import styled from 'styled-components'
 import { theme } from 'styles/theme'
+import { convertIntoRGBA } from 'utils/color/convertIntoRGBA'
 
+type StyledLabelProps = { color?: string; fontSize?: string }
+type StyledInputProps = {
+  width?: string
+  height?: string
+  border?: string
+  borderRadius?: string
+  backgroundColor?: string
+}
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  labelStyles?: StyledLabelProps
+  inputStyles?: StyledInputProps
   label?: string
   error?: FieldError | undefined
   description?: string
@@ -12,16 +23,16 @@ type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
 }
 
 export const InputField: FC<InputFieldProps> = props => {
-  const { label, registration, error, ...inputAttributes } = props
+  const { labelStyles, inputStyles, label, registration, error, ...inputAttributes } = props
 
   return (
     <div>
-      <label>
+      <StyledLabel {...labelStyles}>
         {label}
-        <div>
+        <StyledInputWrapper {...inputStyles}>
           <input {...registration} {...inputAttributes} />
-        </div>
-      </label>
+        </StyledInputWrapper>
+      </StyledLabel>
       {error?.message && (
         <div role="alert" aria-label={error.message}>
           {error.message}
@@ -31,12 +42,25 @@ export const InputField: FC<InputFieldProps> = props => {
   )
 }
 
-type StyledLabelProps = { color?: string }
-
 const StyledLabel = styled.label<StyledLabelProps>`
   color: ${props => props.color};
-  font-size: ${({ theme }) => theme.fontSizes.size_16};
+  font-size: ${props => props.fontSize};
 `
 StyledLabel.defaultProps = {
   color: theme.colors.chocolate,
+  fontSize: theme.fontSizes.size_16,
+}
+const StyledInputWrapper = styled.div<StyledInputProps>`
+  input {
+    height: ${props => props.height};
+    border: ${props => props.border};
+    border-radius: ${props => props.borderRadius};
+    background-color: ${props => props.backgroundColor};
+  }
+`
+StyledInputWrapper.defaultProps = {
+  height: '40px',
+  border: `solid 1px ${theme.colors.chocolate}`,
+  borderRadius: '2px',
+  backgroundColor: convertIntoRGBA(theme.colors.white, 0.7),
 }
