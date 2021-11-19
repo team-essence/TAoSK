e6c6f2799cc1b55a3ca2d537a15fd468fc195682
h-jun01 <jyun.0110@icloud.com>
2021-11-19 16:54:10 +0900

    feat: loading

--- frontend/src/context/AuthProvider.tsx
+++ frontend/src/context/AuthProvider.tsx
@@ -1,57 +1,44 @@
-import React, {
-  FC,
-  createContext,
-  useState,
-  useContext,
-  useEffect,
-} from 'react';
-import { auth } from 'utils/lib/firebase/firebaseInitial';
-import { onAuthStateChanged, User } from 'firebase/auth';
+import React, { FC, createContext, useState, useContext, useEffect } from 'react'
+import { auth } from 'utils/lib/firebase/firebaseInitial'
+import { onAuthStateChanged, User } from 'firebase/auth'
+// import './test.css'
 
-// import { Loading } from 'components/loading/Loading';
+import { Loading } from 'components/ui/loading/Loading'
 
 type AuthContextProps = {
-  currentUser: User | null;
-  loading: boolean;
-};
+  currentUser: User | null
+  loading: boolean
+}
 
 const AuthContext = createContext<AuthContextProps>({
   currentUser: null,
   loading: true,
-});
+})
 
 export const useAuthContext = () => {
-  return useContext(AuthContext);
-};
+  return useContext(AuthContext)
+}
 
 export const AuthProvider: FC = ({ children }) => {
-  const [loading, setLoading] = useState(true);
-  const [currentUser, setCurrentUser] = useState<User | null>(null);
-  const value = { currentUser, loading };
+  const [loading, setLoading] = useState(true)
+  const [currentUser, setCurrentUser] = useState<User | null>(null)
+  const value = { currentUser, loading }
 
   useEffect(() => {
-    const unsubscribed = onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user);
-      setLoading(false);
-    });
+    const unsubscribed = onAuthStateChanged(auth, user => {
+      setCurrentUser(user)
+      setLoading(false)
+    })
 
     return () => {
-      unsubscribed();
-    };
-  }, []);
+      unsubscribed()
+    }
+  }, [])
 
-  if (loading) {
-    return (
-      <div>
-        <p>ローディング中</p>
-      </div>
-    );
-  } else {
-    return (
-      <AuthContext.Provider
-        value={{ currentUser: value.currentUser, loading: value.loading }}>
-        {!loading && children}
-      </AuthContext.Provider>
-    );
-  }
-};
+  if (loading) return <Loading />
+  return (
+    <AuthContext.Provider value={{ currentUser: value.currentUser, loading: value.loading }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  )
+}
