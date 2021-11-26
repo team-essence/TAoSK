7bee0fe8d5b562c2384538c6ff2975fa825250bb
h-jun01 <jyun.0110@icloud.com>
2021-11-26 14:15:06 +0900

    fix: Modify the module

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import React, { FC } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from 'routes/AppRoutes'
+import { AppProvider } from 'providers/AppProvider'
+import { GlobalStyle } from 'styles/globalStyle'
+import { ToastContainer } from 'react-toastify'
 
 const App: FC = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <AppProvider>
+      <GlobalStyle />
+      <ToastContainer />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </AppProvider>
   )
 }
 
