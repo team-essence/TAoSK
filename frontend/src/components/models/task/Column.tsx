ba0abbe3ede94007245a5e7d180e4f427acfa1db
h-jun01 <jyun.0110@icloud.com>
2021-11-30 22:17:02 +0900

    feat: spread image

--- frontend/src/components/models/task/Column.tsx
+++ frontend/src/components/models/task/Column.tsx
@@ -2,7 +2,9 @@ import React, { FC } from 'react'
 import { Draggable, Droppable } from 'react-beautiful-dnd'
 import { List } from 'types/list'
 import { DropType } from 'consts/dropType'
+import { calculateMinSizeBasedOnFigmaWidth } from 'utils/calculateSizeBasedOnFigma'
 import { TaskList } from 'components/models/task/TaskList'
+import styled from 'styled-components'
 
 type Props = {
   className?: string
@@ -19,22 +21,22 @@ export const Column: FC<Props> = ({ id, title, tasks, listIndex, listLength }) =
       isDragDisabled={listIndex === 0 || listLength - 1 === listIndex}>
       {provided => (
         <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-          <h2>{title}</h2>
           {/* <button onClick={() => handleAddTask(listIndex + 1)}>追加</button> */}
           <Droppable droppableId={String(listIndex)} type={DropType.TASK}>
             {listProvided => (
-              <ul
+              <StyledColumnContainer
                 ref={listProvided.innerRef}
                 {...listProvided.droppableProps}
                 style={{
-                  width: '400px',
-                  border: 'solid',
+                  // width: '400px',
+                  // border: 'solid',
                   minHeight: '300px',
-                  padding: '20px',
+                  // padding: '20px',
                 }}>
+                <h2>{title}</h2>
                 <TaskList tasks={tasks} listIndex={listIndex} listLength={listLength} />
                 {listProvided.placeholder}
-              </ul>
+              </StyledColumnContainer>
             )}
           </Droppable>
         </div>
@@ -42,3 +44,9 @@ export const Column: FC<Props> = ({ id, title, tasks, listIndex, listLength }) =
     </Draggable>
   )
 }
+
+const StyledColumnContainer = styled.ul`
+  width: ${calculateMinSizeBasedOnFigmaWidth(270)};
+  border: 1px solid ${({ theme }) => theme.COLORS.MONDO};
+  border-radius: 3px;
+`
