082624dd5022dffedef5158a6840f1ef46e85dc1
roll1226 <tomo_yasu_4724@icloud.com>
2021-11-30 20:00:56 +0900

    feat: my page background image

--- frontend/src/pages/mypage/MyPage.tsx
+++ frontend/src/pages/mypage/MyPage.tsx
@@ -2,6 +2,8 @@ import React, { FC, useEffect } from 'react'
 import { Navigate, useParams } from 'react-router-dom'
 import { useAuthContext } from 'providers/AuthProvider'
 import { useGetUserLazyQuery } from './mypage.gen'
+import styled, { css } from 'styled-components'
+import { ProjectListHeader } from 'components/ui/header/ProjectListHeader'
 
 export const MyPage: FC = () => {
   const { currentUser } = useAuthContext()
@@ -17,9 +19,29 @@ export const MyPage: FC = () => {
   if (currentUser.uid !== id) return <Navigate to="/" />
 
   return (
-    <div>
+    <>
+      <ProjectListHeader />
+
       <p>マイページ</p>
-      <pre>{JSON.stringify(userQuery.data?.user, null, '\t')}</pre>
-    </div>
+      <pre style={{ color: '#fff' }}>{JSON.stringify(userQuery.data?.user, null, '\t')}</pre>
+      <StyledMyPageBackground />
+    </>
   )
 }
+
+const StyledMyPageBackground = styled.div`
+  ${({ theme }) => css`
+    z-index: ${theme.Z_INDEX.INDEX_MINUS_1};
+    top: ${theme.HEADER_HEIGHT};
+    height: calc(100vh - ${theme.HEADER_HEIGHT});
+  `};
+
+  position: fixed;
+  left: 0;
+  width: 100vw;
+  background: url('/images/my-page_background.webp');
+  background-attachment: fixed;
+  background-position: cover;
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+`
