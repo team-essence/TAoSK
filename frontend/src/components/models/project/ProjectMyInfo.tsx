c2d0be71392df65e5c0c3dee4cac2f18116712d7
roll1226 <tomo_yasu_4724@icloud.com>
2021-12-04 20:26:58 +0900

    feat: my status bar components

--- frontend/src/components/models/project/ProjectMyInfo.tsx
+++ frontend/src/components/models/project/ProjectMyInfo.tsx
@@ -1,9 +1,9 @@
-import { occupationList } from 'consts/occupationList'
-import { BAR_TYPE } from 'consts/bar'
 import React, { FC } from 'react'
+import { occupationList } from 'consts/occupationList'
 import styled, { css } from 'styled-components'
 import { calculateMinSizeBasedOnFigmaWidth } from 'utils/calculateSizeBasedOnFigma'
 import exp from 'utils/exp/exp'
+import { ProjectMyBars } from './ProjectMyBars'
 
 type Props = {
   className?: string
@@ -43,38 +43,7 @@ export const ProjectMyInfo: FC<Props> = ({
         </StyledLevelContainer>
       </StyledUserContainer>
 
-      <StyledStatusesContainer>
-        <StyledStatusHPContainer>
-          <h4>HP</h4>
-
-          <StyledStatusBackground color={BAR_TYPE.HP_BG} width={133}>
-            <StyledStatusBar color={BAR_TYPE.HP} width={129} rate={hp} isSilver={true}>
-              <p>{hp}/100</p>
-            </StyledStatusBar>
-          </StyledStatusBackground>
-        </StyledStatusHPContainer>
-
-        <StyledStatusMPContainer>
-          <h4>MP</h4>
-
-          <StyledStatusBackground color={BAR_TYPE.MP_BG} width={133}>
-            <StyledStatusBar color={BAR_TYPE.MP} width={129} rate={mp} isSilver={true}>
-              <p>{mp}/100</p>
-            </StyledStatusBar>
-          </StyledStatusBackground>
-        </StyledStatusMPContainer>
-
-        <StyledStatusEXPContainer>
-          <h4>EXP</h4>
-
-          <StyledStatusBackground color={BAR_TYPE.EXP_BG} width={293}>
-            <StyledStatusBar
-              color={BAR_TYPE.EXP}
-              width={289}
-              rate={exp.toRemainderExp(totalExp)}></StyledStatusBar>
-          </StyledStatusBackground>
-        </StyledStatusEXPContainer>
-      </StyledStatusesContainer>
+      <ProjectMyBars hp={hp} mp={mp} totalExp={totalExp} />
 
       <StyledProjectMyInfoBackground
         src="/svg/project-detail-my-info_background.svg"
@@ -192,111 +161,4 @@ const StyledProjectMyInfoBackground = styled.img`
   width: ${calculateMinSizeBasedOnFigmaWidth(436)};
   height: ${calculateMinSizeBasedOnFigmaWidth(110)};
   object-fit: contain;
-`
-
-const StyledStatusesContainer = styled.div`
-  position: absolute;
-  top: 69px;
-  left: 112px;
-  display: grid;
-  grid-template-columns: auto auto;
-  grid-template-rows: 1fr 1fr;
-  gap: 5px 0;
-`
-
-const StyledStatusContainer = styled.div`
-  display: grid;
-  grid-template-columns: auto auto;
-  align-items: center;
-  gap: 0 ${calculateMinSizeBasedOnFigmaWidth(2)};
-  font-family: 'Yanone Kaffeesatz', 'Inter', 'BlinkMacSystemFont', 'Hiragino Kaku Gothic ProN',
-    'Hiragino Sans', Meiryo, sans-serif;
-
-  h4 {
-    position: relative;
-    top: ${calculateMinSizeBasedOnFigmaWidth(1)};
-    font-size: ${({ theme }) => theme.FONT_SIZES.SIZE_18};
-    font-weight: ${({ theme }) => theme.FONT_WEIGHTS.BOLD};
-    color: ${({ theme }) => theme.COLORS.WHITE};
-    line-height: 0;
-  }
-`
-
-const StyledStatusHPContainer = styled(StyledStatusContainer)`
-  padding-left: ${calculateMinSizeBasedOnFigmaWidth(7)};
-  justify-content: flex-start;
-`
-const StyledStatusMPContainer = styled(StyledStatusContainer)`
-  justify-content: flex-end;
-`
-const StyledStatusEXPContainer = styled(StyledStatusContainer)`
-  grid-column: 1 / 3;
-`
-
-const StyledStatusBackground = styled.div<{ color: BAR_TYPE; width: number }>`
-  position: relative;
-  border-radius: ${calculateMinSizeBasedOnFigmaWidth(100)};
-  width: ${({ width }) => calculateMinSizeBasedOnFigmaWidth(width)};
-  height: ${calculateMinSizeBasedOnFigmaWidth(12)};
-  background: ${({ color }) => color};
-`
-
-const StyledStatusBar = styled.div<{
-  color: BAR_TYPE
-  width: number
-  rate: number
-  isSilver?: boolean
-}>`
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: ${({ width }) => calculateMinSizeBasedOnFigmaWidth(width)};
-  height: ${calculateMinSizeBasedOnFigmaWidth(12)};
-
-  p {
-    position: absolute;
-    bottom: -5px;
-    right: ${calculateMinSizeBasedOnFigmaWidth(1)};
-
-    ${({ theme }) => css`
-      width: ${calculateMinSizeBasedOnFigmaWidth(58)};
-      text-align: center;
-      z-index: ${theme.Z_INDEX.INDEX_2};
-      font-size: ${theme.FONT_SIZES.SIZE_16};
-      color: ${theme.COLORS.WHITE};
-      font-weight: ${theme.FONT_WEIGHTS.BOLD};
-      background: linear-gradient(
-        0deg,
-        ${theme.COLORS.MINE_SHAFT},
-        ${theme.COLORS.MINE_SHAFT} 100%
-      );
-      background-size: 200% 200%;
-      -webkit-background-clip: text;
-      -webkit-text-stroke: 3px transparent;
-    `}
-  }
-
-  &::before {
-    content: '';
-    border-radius: ${calculateMinSizeBasedOnFigmaWidth(100)};
-    position: absolute;
-    width: 100%;
-    height: 100%;
-
-    ${({ theme, isSilver }) => css`
-      ${isSilver &&
-      css`
-        background: ${theme.COLORS.SILVER};
-      `}
-    `}
-  }
-
-  &::after {
-    content: '';
-    border-radius: ${calculateMinSizeBasedOnFigmaWidth(100)};
-    background: ${({ color }) => color};
-    position: absolute;
-    width: ${({ rate }) => rate}%;
-    height: 100%;
-  }
 `
