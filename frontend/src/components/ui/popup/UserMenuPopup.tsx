dfd38b3f9f1c76104a0196b8f5b8db009922760d
h-jun01 <jyun.0110@icloud.com>
2021-12-07 17:19:22 +0900

    feat: Add opacity on hover.

--- frontend/src/components/ui/popup/UserMenuPopup.tsx
+++ frontend/src/components/ui/popup/UserMenuPopup.tsx
@@ -57,7 +57,7 @@ export const UserMenuPopup: FC<Props> = ({
 
       <StyledAccountRelationshipContainer>
         <p>
-          <Link to={'/mypage/' + uid}>マイページ</Link>
+          <Link to={`/mypage/${uid}`}>マイページ</Link>
         </p>
         {/* TODO: モーダルがきたらonClickを追加 */}
         <button>アカウント設定</button>
@@ -80,11 +80,9 @@ const StyledUserInfoContainer = styled.div`
   align-items: center;
   gap: 0 ${calculateMinSizeBasedOnFigmaWidth(6)};
 `
-
 const StyledUserIconImageContainer = styled.div`
   width: ${calculateMinSizeBasedOnFigmaWidth(40)};
   height: ${calculateMinSizeBasedOnFigmaWidth(40)};
-
   img {
     width: ${calculateMinSizeBasedOnFigmaWidth(40)};
     height: ${calculateMinSizeBasedOnFigmaWidth(40)};
@@ -94,15 +92,12 @@ const StyledUserIconImageContainer = styled.div`
     border: solid 1px ${({ theme }) => theme.COLORS.BRANDY};
   }
 `
-
 const StyledUserInfo = styled.div``
-
 const StyledUserNameContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 0 ${calculateMinSizeBasedOnFigmaWidth(8)};
 `
-
 const StyledUserName = styled.p`
   font-size: ${({ theme }) => theme.FONT_SIZES.SIZE_14};
   font-weight: ${({ theme }) => theme.FONT_WEIGHTS.MEDIUM};
@@ -113,7 +108,6 @@ const StyledUserName = styled.p`
   text-overflow: ellipsis;
   white-space: nowrap;
 `
-
 const StyledLevelContainer = styled.div`
   width: ${calculateMinSizeBasedOnFigmaWidth(45)};
   height: ${calculateMinSizeBasedOnFigmaWidth(18)};
@@ -123,7 +117,6 @@ const StyledLevelContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-
   p {
     font-size: ${({ theme }) => theme.FONT_SIZES.SIZE_12};
     color: ${({ theme }) => theme.COLORS.WHITE};
@@ -139,26 +132,25 @@ const StyledUserUid = styled.p`
   text-overflow: ellipsis;
   white-space: nowrap;
 `
-
 const StyledAccountRelationshipContainer = styled.div`
   padding: ${calculateMinSizeBasedOnFigmaWidth(8)} ${calculateMinSizeBasedOnFigmaWidth(24)};
   border-bottom: solid ${calculateMinSizeBasedOnFigmaWidth(1)} ${({ theme }) => theme.COLORS.SILVER};
   display: flex;
   flex-direction: column;
   gap: ${calculateMinSizeBasedOnFigmaWidth(8)} 0;
-
   p {
     font-size: ${({ theme }) => theme.FONT_SIZES.SIZE_14};
     font-weight: ${({ theme }) => theme.FONT_WEIGHTS.MEDIUM};
     color: ${({ theme }) => theme.COLORS.FONT.BLACK};
     cursor: pointer;
-
+    &:hover {
+      opacity: 0.6;
+    }
     a {
       display: block;
       width: 100%;
     }
   }
-
   button {
     display: block;
     width: 100%;
@@ -166,17 +158,22 @@ const StyledAccountRelationshipContainer = styled.div`
     font-weight: ${({ theme }) => theme.FONT_WEIGHTS.MEDIUM};
     color: ${({ theme }) => theme.COLORS.FONT.BLACK};
     text-align: left;
+    &:hover {
+      opacity: 0.6;
+    }
   }
 `
 
 const StyledLSignOutContainer = styled.div`
   padding: ${calculateMinSizeBasedOnFigmaWidth(8)} ${calculateMinSizeBasedOnFigmaWidth(24)};
-
   button {
     font-size: ${({ theme }) => theme.FONT_SIZES.SIZE_14};
     font-weight: ${({ theme }) => theme.FONT_WEIGHTS.MEDIUM};
     color: ${({ theme }) => theme.COLORS.FONT.BLACK};
     width: 100%;
     text-align: left;
+    &:hover {
+      opacity: 0.6;
+    }
   }
 `
