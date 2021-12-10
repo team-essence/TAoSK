d20f584562c7c0b1fdfd208a83d658aff335d430
roll1226 <tomo_yasu_4724@icloud.com>
2021-12-10 17:32:29 +0900

    fix: 無理矢理プレゼンスの修正

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,16 +1,39 @@
-import React, { FC } from 'react'
-import { useRoutes } from 'react-router-dom'
+import React, { FC, useEffect, useState } from 'react'
+import { useParams, useRoutes } from 'react-router-dom'
 import { useAuthContext } from 'providers/AuthProvider'
 import { NotFound } from 'pages/notFound/NotFound'
 import { protectedRoutes } from './Protected'
 import { publicRoutes } from './Public'
+import logger from 'utils/debugger/logger'
+import { useUpdateOnlineFlagMutation } from 'pages/projectDetail/projectDetail.gen'
 
 export const AppRoutes: FC = () => {
   const { currentUser } = useAuthContext()
+  const [projectId, setProjectId] = useState('')
+  const { id } = useParams()
   const commonRoutes = [{ path: '*', element: <NotFound /> }]
   const routes = currentUser ? protectedRoutes : publicRoutes
 
+  const [updateOnlineFlag] = useUpdateOnlineFlagMutation()
+
   const element = useRoutes([...routes, ...commonRoutes])
+
+  useEffect(() => {
+    logger.debug(location.pathname)
+    if (!currentUser) return
+    if (location.pathname.includes('projects'))
+      return setProjectId(location.pathname.replace('/projects/', ''))
+
+    if (projectId) {
+      updateOnlineFlag({
+        variables: {
+          id: currentUser.uid,
+          project_id: projectId,
+          isOnline: false,
+        },
+      })
+    }
+  }, [currentUser, location.pathname])
 
   return <>{element}</>
 }
