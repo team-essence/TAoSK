72c6c82a7bae3e5863372d5c2537399c6ca5fc4f
roll1226 <tomo_yasu_4724@icloud.com>
2021-12-12 22:44:06 +0900

    feat: forFeature

--- api/src/occupations/occupations.module.ts
+++ api/src/occupations/occupations.module.ts
@@ -3,8 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Occupation } from './occupation';
 import { OccupationsService } from './occupations.service';
 import { OccupationsResolver } from './occupations.resolver';
+import { User } from 'src/users/user';
 @Module({
-  imports: [TypeOrmModule.forFeature([Occupation])],
+  imports: [
+    TypeOrmModule.forFeature([Occupation]),
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [OccupationsService, OccupationsResolver],
 })
 export class OccupationsModule {}
