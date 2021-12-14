b9e67f84324f9dc736b82d376e8f90f43ade4a48
roll1226 <tomo_yasu_4724@icloud.com>
2021-12-14 10:01:20 +0900

    feat: monster seed

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,8 +1,14 @@
 import { PrismaClient } from '@prisma/client';
+import { monsters } from './lib/monster';
 import { occupations } from './lib/occupation';
 
 const prisma = new PrismaClient();
-async function main() {
+const main = async () => {
+  await occupationSeeder();
+  await monsterSeeder();
+};
+
+const occupationSeeder = async () => {
   for (const occupation of occupations) {
     await prisma.occupations.upsert({
       where: {
@@ -12,7 +18,41 @@ async function main() {
       update: occupation,
     });
   }
-}
+};
+
+const monsterSeeder = async () => {
+  for (const monster of monsters) {
+    await prisma.monsters.upsert({
+      where: {
+        id: monster.id,
+      },
+
+      create: {
+        id: monster.id,
+        name: monster.name,
+        type: monster.type,
+        story: monster.story,
+
+        specie: {
+          connectOrCreate: {
+            where: {
+              name: monster.specie.name,
+            },
+
+            create: monster.specie,
+          },
+        },
+      },
+
+      update: {
+        id: monster.id,
+        name: monster.name,
+        type: monster.type,
+        story: monster.story,
+      },
+    });
+  }
+};
 
 main()
   .catch((e) => {
