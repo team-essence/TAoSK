fa0a9e6ceabb73c46084bd20007aa285eb70af23
roll1226 <tomo_yasu_4724@icloud.com>
2022-01-05 03:08:21 +0900

    feat: 背景の切り替え #338

--- frontend/src/components/models/monster/MonsterAvatar.tsx
+++ frontend/src/components/models/monster/MonsterAvatar.tsx
@@ -1,22 +1,63 @@
-import React, { Suspense, FCX } from 'react'
+import React, { Suspense, FCX, useState } from 'react'
 import { Environment, OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { CgAxesHelper } from 'components/models/cg/CgAxesHelper'
 import { CANVAS_BACKGROUND_IMG, CgCanvasWrapper } from '../cg/CgCanvasWrapper'
 import { CgAvatarContainer } from '../cg/CgAvatarContainer'
-import Model from './Dragon1125'
+import Model, { MonsterActionName } from './Dragon1125'
+import styled from 'styled-components'
 
 export const MonsterAvatar: FCX = ({ className }) => {
+  const [monsterActionName, setMonsterActionName] = useState<MonsterActionName>('idle')
+
+  const [canvasBackground, setCanvasBackground] = useState<CANVAS_BACKGROUND_IMG>(
+    CANVAS_BACKGROUND_IMG.MORNING,
+  )
+
+  const handleChangeCanvasBackground = (backgroundImage: CANVAS_BACKGROUND_IMG) => {
+    setCanvasBackground(backgroundImage)
+    switch (backgroundImage) {
+      case CANVAS_BACKGROUND_IMG.NORMAL:
+      case CANVAS_BACKGROUND_IMG.AFTERNOON:
+        setMonsterActionName('eat')
+        break
+
+      case CANVAS_BACKGROUND_IMG.NIGHT:
+        setMonsterActionName('sleeping')
+        break
+
+      default:
+        setMonsterActionName('idle')
+        break
+    }
+  }
+
   return (
     <CgAvatarContainer className={className}>
-      <CgCanvasWrapper canvasBackgroundImg={CANVAS_BACKGROUND_IMG.MORNING}>
+      <CgCanvasWrapper canvasBackgroundImg={canvasBackground}>
+        <StyledButtonContainer>
+          <StyledButton onClick={() => handleChangeCanvasBackground(CANVAS_BACKGROUND_IMG.MORNING)}>
+            1
+          </StyledButton>
+          <StyledButton onClick={() => handleChangeCanvasBackground(CANVAS_BACKGROUND_IMG.NORMAL)}>
+            2
+          </StyledButton>
+          <StyledButton
+            onClick={() => handleChangeCanvasBackground(CANVAS_BACKGROUND_IMG.AFTERNOON)}>
+            3
+          </StyledButton>
+          <StyledButton onClick={() => handleChangeCanvasBackground(CANVAS_BACKGROUND_IMG.NIGHT)}>
+            4
+          </StyledButton>
+        </StyledButtonContainer>
+
         <Canvas camera={{ fov: 3.8, position: [12, 4, 12] }}>
           <OrbitControls enablePan={false} />
 
           <Suspense fallback={null}>
             <ambientLight intensity={2} />
             <spotLight intensity={2} position={[30, 30, 50]} angle={0.2} penumbra={1} castShadow />
-            <Model position={[0, -0.3, 0]} />
+            <Model actionName={monsterActionName} position={[0, -0.3, 0]} />
             <Environment preset="sunset" />
             <CgAxesHelper />
           </Suspense>
@@ -25,3 +66,23 @@ export const MonsterAvatar: FCX = ({ className }) => {
     </CgAvatarContainer>
   )
 }
+
+const StyledButtonContainer = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  gap: 12px 0;
+  top: 10px;
+  left: 10px;
+  z-index: 5;
+`
+
+const StyledButton = styled.div`
+  width: 20px;
+  height: 20px;
+  background: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`
